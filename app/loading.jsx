2f2dcce7b8cc548f6058b96049831f7f68dba59d
Joshua Lane <67156874+lanejm@users.noreply.github.com>
2024-11-19 13:31:44 -0500

    react spinner

--- app/loading.jsx
+++ app/loading.jsx
@@ -1,5 +1,19 @@
+"use client";
+import ClipLoader from "react-spinners/ClipLoader";
+
+const override = {
+  display: "block",
+  margin: "100px auto",
+};
 const LoadingPage = () => {
-  return <div>Loading...</div>;
+  return (
+    <ClipLoader
+      color="#3b82f6"
+      cssOverride={override}
+      size={150}
+      aria-label="Loading Spinner"
+    />
+  );
 };
 
 export default LoadingPage;
