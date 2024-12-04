34465151f97c8eba298b335e78426606a4a0625b
Joshua Lane <67156874+lanejm@users.noreply.github.com>
2024-12-04 15:48:05 -0500

    check bookmark status

--- components/BookmarkButton.jsx
+++ components/BookmarkButton.jsx
@@ -1,5 +1,7 @@
 "use client";
+import { useState, useEffect } from "react";
 import bookmarkProperty from "@/app/actions/bookmarkProperty";
+import checkBookmarkStatus from "@/app/actions/checkBookmarkStatus";
 import { toast } from "react-toastify";
 import { FaBookmark } from "react-icons/fa";
 import { useSession } from "next-auth/react";
@@ -8,6 +10,21 @@ const BookmarkButton = ({ property }) => {
   const { data: session } = useSession();
   const userId = session?.user?.id;
 
+  const [isBookmarked, setIsBookmarked] = useState(false);
+  const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+    if (!userId) {
+      setLoading(false);
+      return;
+    }
+    checkBookmarkStatus(property._id).then((res) => {
+      if (res.error) toast.error(res.error);
+      if (res.isBookmarked) setIsBookmarked(res.isBookmarked);
+      setLoading(false);
+    });
+  }, [property._id, userId, checkBookmarkStatus]);
+
   const handleClick = async () => {
     if (!userId) {
       toast.error("Please sign in to bookmark a listing");
@@ -15,10 +32,21 @@ const BookmarkButton = ({ property }) => {
     }
     bookmarkProperty(property._id).then((res) => {
       if (res.error) return toast.error(res.error);
+      setIsBookmarked(res.isBookmarked);
       toast.success(res.message);
     });
   };
-  return (
+  if (loading) {
+    return <p className="text-center">Loading...</p>;
+  }
+  return isBookmarked ? (
+    <button
+      className="bg-red-500 hover:bg-red-600 text-white font-bold w-full py-2 px-4 rounded-full flex items-center justify-center"
+      onClick={handleClick}
+    >
+      <FaBookmark className="mr-2"></FaBookmark> Remove Bookmark
+    </button>
+  ) : (
     <button
       className="bg-blue-500 hover:bg-blue-600 text-white font-bold w-full py-2 px-4 rounded-full flex items-center justify-center"
       onClick={handleClick}
