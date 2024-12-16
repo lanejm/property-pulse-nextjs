5d0c2366efca0d2417b5d3250b5b45d61e0032fa
Joshua Lane <67156874+lanejm@users.noreply.github.com>
2024-12-16 14:48:16 -0500

    pagination component

--- app/properties/page.jsx
+++ app/properties/page.jsx
@@ -1,13 +1,15 @@
 import PropertyCard from "@/components/PropertyCard";
 import connectDB from "@/config/database";
 import Property from "@/models/Property";
+import Pagination from "@/components/Pagination";
 
-const PropertiesPage = async ({ searchParams: { page = 1, pageSize = 2 } }) => {
+const PropertiesPage = async ({ searchParams: { page = 1, pageSize = 9 } }) => {
   await connectDB();
   const skip = (page - 1) * pageSize;
   const total = await Property.countDocuments({});
   //lean optimizes query performance by returning JS objects instead of mongoose docs - READ ONLY
   const properties = await Property.find({}).skip(skip).limit(pageSize);
+  const showPagination = total > pageSize;
   return (
     <section className="px-4 py-6">
       <div className="container-xl lg:container m-auto px-4 py-6">
@@ -22,6 +24,13 @@ const PropertiesPage = async ({ searchParams: { page = 1, pageSize = 2 } }) => {
               ></PropertyCard>
             ))}
           </div>
+        )}
+        {showPagination && (
+          <Pagination
+            page={parseInt(page)}
+            pageSize={parseInt(pageSize)}
+            totalItems={total}
+          />
         )}
       </div>
     </section>
